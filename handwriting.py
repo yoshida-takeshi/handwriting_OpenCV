0b1c98ebf15a9a16f6f71606da96a72a737b2007
yoshida-takeshi <noroma.club@gmail.com>
2018-01-29 22:29:44 +0900

    modify

--- handwriting.py
+++ handwriting.py
@@ -8,6 +8,7 @@ color = (0, 0, 0)
 #ペンの太さ
 thickness = 10
 
+#筆跡記憶配列
 dtype_plot = [("x", "i2"), ("y", "i2")]
 plot = np.zeros(1024, dtype=dtype_plot)
 plot_size = 0
@@ -39,51 +40,36 @@ def callback(event, x, y, flags, param):
     #マウスの左ボタンがクリックされたとき
     if event == cv2.EVENT_LBUTTONDOWN:
         sx, sy = x, y
-        print(x,y)
+        print(plot_size,x,y)
         plot[plot_size] = (x,y)
         plot_size += 1
+
     #マウスの左ボタンがクリックされていて、マウスが動いたとき
     if (flags & cv2.EVENT_FLAG_LBUTTON) and event == cv2.EVENT_MOUSEMOVE:
         cv2.line(img, (sx, sy), (x, y), color, thickness)
         sx, sy = x, y
-        print(x,y)
+        print(plot_size,x,y)
         plot[plot_size] = (x,y)
         plot_size += 1
 
-# トラックバーを変更したらペンを変更
-#def changePencil(pos):
-#    global color, thickness
-#    r = cv2.getTrackbarPos("R", "img")
-#    g = cv2.getTrackbarPos("G", "img")
-#    b = cv2.getTrackbarPos("B", "img")
-#    color = (b, g, r)
-#    thickness = cv2.getTrackbarPos("T", "img")
-
-##画像を読み込む
-#img = cv2.imread("img.jpg")
+        
+#新しいウィンドウを開く
 img = np.zeros((512, 512, 3), np.uint8)
 img[:,:,:]=(255,255,255)
-
-#ウィンドウの名前を設定
-#cv2.namedWindow("img", cv2.WINDOW_NORMAL)
 cv2.namedWindow("img")
 
 #マウス操作のコールバック関数の設定
 cv2.setMouseCallback("img", callback)
 
-#トラックバーのコールバック関数の設定
-#cv2.createTrackbar("R", "img", 0, 255, changePencil)
-#cv2.createTrackbar("G", "img", 0, 255, changePencil)
-#cv2.createTrackbar("B", "img", 0, 255, changePencil)
-#cv2.createTrackbar("T", "img", 1, 100, changePencil)
-
 while(1):
     cv2.imshow("img", img)
     k = cv2.waitKey(1)
+
     #Escキーを押すと終了
     if k == 27:
         break
-    #sを押すと画像を保存
+
+    #sを押すとデータを保存
     if k == ord("s"):
         path_w = raw_input('filename=> ')
         f = open(path_w, 'w')
@@ -91,8 +77,8 @@ while(1):
             print plot[i]
             f.write(str(plot[i]))
         print "write: ",path_w
-        #cv2.imwrite("painted.png", img)
-        #break
+
+    #cを押すとデータクリア
     if k == ord("c"):
         img[:,:,:]=(255,255,255)
         plot_size=0
